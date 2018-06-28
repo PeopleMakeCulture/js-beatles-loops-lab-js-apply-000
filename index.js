39f5ad58551cd31c0eb879ab066c126b92f69bf1
Jing Cao <jingcao.yale@gmail.com>
2018-06-28 15:08:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-function theBeatlesPlay (musicians, instruments){
+function theBeatlesPlay (musician, instrument){
   //create an empty array stored in a variable
-  var hePlaysThis = ['this']
+  var theyPlayThese = [];
+
   //contain a for loop which loops over array of musicians
+
+  for (i = 0; i<musicians.length; i++) {
+    var hePlaysThis = '${musician} plays ${instrument}';
+  }
   //NOTE: be careful about value for counter variable (Hint: think about first index of array)
   //first time through the loop should create a string
   //add string to empty array
   //function should return array of strings
-  return hePlaysThis
+  return theyPlayThese 
 
 }
 
