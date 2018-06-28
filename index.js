f68b3bf4dca37e212978bf58baceae0b1cf55b1f
Jing Cao <jingcao.yale@gmail.com>
2018-06-28 14:43:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 function theBeatlesPlay (musicians, instruments){
   //create an empty array stored in a variable
-  var hePlaysThis = []
+  var hePlaysThis = ['this']
   //contain a for loop which loops over array of musicians
   //NOTE: be careful about value for counter variable (Hint: think about first index of array)
-  //first time through the loop should create a string 
+  //first time through the loop should create a string
   //add string to empty array
   //function should return array of strings
-  return hePlaysThis 
+  return hePlaysThis
+
 }
+
+console.log(theBeatlesPlay())
